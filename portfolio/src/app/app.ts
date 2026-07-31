4b4bf3238453a79938697a77fca1540fbd1a9792
Damia <dsargent@student.neumont.edu>
2026-07-30 18:09:39 -0600

    Basics implemented

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from './nav-bar/nav-bar';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('portfolio');
---
+++ portfolio/src/app/nav-bar/nav-bar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.scss',
+  standalone: true,
+})
+export class NavBar {}
